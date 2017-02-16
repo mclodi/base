0b3f7031ed5ec1b4b8d9356efcd5d2370723002c
Mark Clodi <mclodi@farewaystores.com>
2017-02-16 15:43:29 -0600

    Basic Hello World page created.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World!</title>
+</head>
+<body>
+
+<h1>This is a hello World page</h1>
+<p>Welcome!</p>
+
+<?php
+	echo "Hello world!";
+?>
+
+</body>
+</html>
